ee2be7b8bf72a6552ac8d78cb75a4524ad7d5b3b
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2020-06-28 11:00:40 +0700

    init first data record in StringBootOnline2Application file

--- src/main/java/com/example/springbootonline2/SpringbootOnline2Application.java
+++ src/main/java/com/example/springbootonline2/SpringbootOnline2Application.java
@@ -1,10 +1,24 @@
 package com.example.springbootonline2;
 
+import com.example.springbootonline2.domain.Employee;
+import com.example.springbootonline2.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootOnline2Application {
+
+	@Bean
+	public CommandLineRunner init(EmployeeRepository employeeRepository) {
+		return (args -> {
+			Employee employee = new Employee();
+			employee.setFirstName("john");
+			employee.setLastName("doe");
+			employeeRepository.save(employee);
+		});
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootOnline2Application.class, args);
---
+++ src/main/java/com/example/springbootonline2/domain/Employee.java
@@ -0,0 +1,37 @@
+package com.example.springbootonline2.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Id @GeneratedValue
+    @Column(name = "ID") private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "FIRST_NAME") private  String firstName;
+    @Column(name = "LAST_NAME") private String lastName;
+}
